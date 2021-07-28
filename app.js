6d8004a04a2cedd4be3790d994f332f3b74b6844
Luiz Gustavo <lgsouza.sp@gmail.com>
2021-07-28 12:24:37 -0300

    Estrutura básica

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+// const db = require('./dbConnection')
+
+const app = express()
+
+// Configurações do Express
+// ---------------------------------------------
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('./public'))
+
+// Rotas
+// ---------------------------------------------
+app.get('/', (req, res) => {
+
+  res.render('index')
+
+});
+
+// Start Server
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor rodando com express')
+  console.log('Pressione CTRL+C para encerrar')
+});
